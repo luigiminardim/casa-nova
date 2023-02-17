d7a9dad607efa86d8dca13e1fd6067ff2d08db22
Luigi Minardi <luigiminardim@gmail.com>
2023-02-17 18:36:40 -0300

    feat: Mudar nome para chá de casamento.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,14 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <Head>
-          <title>Chá de Casa Nova</title>
-          <meta
-            property="description"
-            content={`
-            Estamos passando por uma grande mudança. Finalmente teremos nossa
-            casinha! (O casamento não foi dessa vez...) Portanto, gostariamos de
-            compartilhar nossa alegria nesse chá de casa nova.`}
-          ></meta>
+          <title>Chá de Casamento</title>
           <link
             rel="shortcut icon"
             href="https://png.pngtree.com/png-vector/20190826/ourmid/pngtree-house-location-icon-png-image_1701248.jpg"
